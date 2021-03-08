1bcd9d29f15b2cbb8a46c8b7aac447d189ce0967
konstantinvl <konstantinvl@inbox.ru>
2021-03-08 22:16:42 +0300

    feat: add min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array===undefined){
+    return 0;
+  } else if (array.length==0){
+    return 0;
+  }else return array.sort((a,b)=>a-b)[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array===undefined){
+    return 0;
+  } else if (array.length==0){
+    return 0;
+  }else return array.sort((a,b)=>b-a)[0];
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array) {  
+  if (array===undefined){
+    return 0;
+  } else if (array.length==0){
+    return 0;
+  }else{
+    let result = array.reduce((sum, current) => sum + current, 0);
+    return (result/array.length);
+  }
 }
